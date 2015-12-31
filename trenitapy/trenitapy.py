a80ab7142adcecb3a4bd957a47e8255417b78e65
sgs00 <g.saviano@gmail.com>
2015-12-31 23:53:04 +0100

    DRY

--- trenitapy/trenitapy.py
+++ trenitapy/trenitapy.py
@@ -6,26 +6,30 @@ from requests.compat import urljoin
 import logging
 
 logger = logging.getLogger(__name__)
-base_url = 'http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/'
 
 
 class TrenitApy:
+
+    @staticmethod
+    def _call(action, *args, fmt='json'):
+        base_url = 'http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno'
+        url = '/'.join((base_url, action, ) + args)
+        response = requests.get(url)
+        if fmt == 'json':
+            data = response.json()
+        else:
+            data = response.text
+        return data
 
     def andamento_treno(self, id_stazione_partenza, numero_treno):
         """
         http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/andamentoTreno/IDStazionePartenza/numeroTreno
         http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/andamentoTreno/S00228/4640
         """
-        url = 'andamentoTreno/{}/{}/'.format(id_stazione_partenza, numero_treno)
-        url = urljoin(base_url, url)
-        response = requests.get(url)
-        return response.json()
+        return self.__class__._call('andamentoTreno', id_stazione_partenza, numero_treno)
 
     def cerca_stazione(self, nome_stazione):
-        url = 'cercaStazione/{}/'.format(nome_stazione)
-        url = urljoin(base_url, url)
-        response = requests.get(url)
-        return response.json()
+        return self.__class__._call('cercaStazione', nome_stazione)
 
 
 if __name__ == '__main__':
